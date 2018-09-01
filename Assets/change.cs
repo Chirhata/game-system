252d17ca3e61d0e11d323969b6a94531df0ef4b7
林　拓也 <flag@r1x73042.kyoto-su.ac.jp>
2018-09-01 23:19:27 +0900

    test

---
+++ Assets/change.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class change : MonoBehaviour
+{
+    // Use this for initialization
+    int count;
+    void Start()
+    {
+        count = timer.c();
+    }
+    void Update()
+    {
+        string nowscene = SceneManager.GetActiveScene().name;
+        if (nowscene == "pose")
+        {
+            if (Input.GetKeyDown("return"))
+            {
+                if (count == 1)
+                {
+                    SceneManager.LoadScene("Stage2");
+                }
+                else if (count == 2)
+                {
+                    SceneManager.LoadScene("Stage3");
+                }
+            }
+        }
+        else if (nowscene == "Start")
+        {
+            if (Input.GetKeyDown("return"))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+}
---
+++ Assets/timer.cs
@@ -0,0 +1,91 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+
+    public float totalTime;
+    float seconds;
+    public static int clear = 0;
+    static int load = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        seconds = (int)totalTime;
+        timerText.text = seconds.ToString();
+        string nowscene = SceneManager.GetActiveScene().name;
+        if (load <= 500)
+        {
+            SceneManager.LoadScene("Start");
+            load = 9999;
+        }
+        else if (nowscene == "SampleScene")
+        {
+            if (totalTime <= 0.0140065)
+            {
+                totalTime = 0;
+                SceneManager.LoadScene("gameover");
+            }
+            else{
+                if (Input.GetKeyDown("return"))
+                {
+                    clear += 1;
+                    {
+                        SceneManager.LoadScene("pose");
+                    }
+                }
+            }
+        }
+        else if (nowscene == "Stage2")
+        {
+            if (totalTime <= 0.0140065)
+            {
+                totalTime = 0;
+                SceneManager.LoadScene("gameover");
+            }
+            else
+            {
+                if (Input.GetKeyDown("return"))
+                {
+                    clear += 1;
+                    {
+                        SceneManager.LoadScene("pose");
+                    }
+                }
+            }
+        }
+        else if (nowscene == "Stage3")
+        {
+            if (totalTime <= 0.0140065)
+            {
+                totalTime = 0;
+                SceneManager.LoadScene("gameover");
+            }
+            else
+            {
+                if (Input.GetKeyDown("return"))
+                {
+                    timerText.text = "0";
+                    SceneManager.LoadScene("gameclear");
+                }
+            }
+        }
+    }
+    public static int c()
+    {
+        return clear;
+    }
+}
