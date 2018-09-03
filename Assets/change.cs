e163a7bc7481e9a6b9c07175185bedc565f2b4d8
林　拓也 <flag@r1x73042.kyoto-su.ac.jp>
2018-09-03 13:27:10 +0900

    sintyoku

--- Assets/change.cs
+++ Assets/change.cs
@@ -8,32 +8,39 @@ using UnityEngine.SceneManagement;
 public class change : MonoBehaviour
 {
     // Use this for initialization
-    int count;
+    int count;//timerでclearの値を入れておくための変数
     void Start()
     {
+        //timerでclearの値をここで使えるようにする
         count = timer.c();
     }
     void Update()
     {
-        string nowscene = SceneManager.GetActiveScene().name;
+        string nowscene = SceneManager.GetActiveScene().name;//プレイヤーがいるシーン名を取得する。ステージの判定などに使う
+        //ポーズ画面（ステージクリア後に遷移する画面）での処理
         if (nowscene == "pose")
         {
+            //ここでの処理はreturnキーに設定してます
             if (Input.GetKeyDown("return"))
             {
+                //第一ステージをクリアしていた場合、returnを押したら第二ステージに進むようにする
                 if (count == 1)
                 {
                     SceneManager.LoadScene("Stage2");
                 }
+                //第二ステージをクリアしていた場合、returnを押したら第三ステージに進むようにする
                 else if (count == 2)
                 {
                     SceneManager.LoadScene("Stage3");
                 }
             }
         }
+        //スタート画面での処理
         else if (nowscene == "Start")
         {
             if (Input.GetKeyDown("return"))
             {
+                //returnキーを押したらゲーム開始（第一ステージに進む）するようにする
                 SceneManager.LoadScene("SampleScene");
             }
         }
--- Assets/timer.cs
+++ Assets/timer.cs
@@ -9,10 +9,10 @@ public class timer : MonoBehaviour
 {
     public Text timerText;
 
-    public float totalTime;
-    float seconds;
-    public static int clear = 0;
-    static int load = 0;
+    public float totalTime;//制限時間。UnityのGameObject (1)で長さは調整できる（望みの時間＋１ぐらいにするとちょうどいい）
+    float seconds;//制限時間の値を格納しておく変数。
+    public static int clear = 0;//ステージクリア回数。ステージをクリアすると１ずつ加算される。３になるとゲームクリア。
+    static int load = 0;//最初にスタート画面から始めるようにするための変数。スタート画面に飛んだ後は使わない。
 
 
     // Use this for initialization
@@ -23,22 +23,26 @@ public class timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        totalTime -= Time.deltaTime;
-        seconds = (int)totalTime;
-        timerText.text = seconds.ToString();
-        string nowscene = SceneManager.GetActiveScene().name;
+        totalTime -= Time.deltaTime;//制限時間をここで減らす
+        seconds = (int)totalTime;//残り時間をint型に変換する
+        timerText.text = seconds.ToString();//残り時間を画面に表示する
+        string nowscene = SceneManager.GetActiveScene().name;//プレイヤーがいるシーン名を取得する。ステージの判定などに使う
+        //始まった瞬間強制的にスタート画面に飛ばし、変な場面から始まらないようにする
         if (load <= 500)
         {
             SceneManager.LoadScene("Start");
             load = 9999;
         }
+        ///////第一ステージでの処理
         else if (nowscene == "SampleScene")
         {
+            //制限時間が来たらゲームオーバー画面に遷移する
             if (totalTime <= 0.0140065)
             {
                 totalTime = 0;
                 SceneManager.LoadScene("gameover");
             }
+            //泥棒を見つけたら（今は仮なのでreturnキーで代用）ポーズ画面に遷移する
             else{
                 if (Input.GetKeyDown("return"))
                 {
@@ -49,8 +53,10 @@ public class timer : MonoBehaviour
                 }
             }
         }
+        ///////第二ステージでの処理
         else if (nowscene == "Stage2")
         {
+            //制限時間が来たらゲームオーバー画面に遷移する
             if (totalTime <= 0.0140065)
             {
                 totalTime = 0;
@@ -58,6 +64,7 @@ public class timer : MonoBehaviour
             }
             else
             {
+                //泥棒を見つけたら（今は仮なのでreturnキーで代用）ポーズ画面に遷移する
                 if (Input.GetKeyDown("return"))
                 {
                     clear += 1;
@@ -67,8 +74,10 @@ public class timer : MonoBehaviour
                 }
             }
         }
+        ///////第三ステージでの処理
         else if (nowscene == "Stage3")
         {
+            //制限時間が来たらゲームオーバー画面に遷移する
             if (totalTime <= 0.0140065)
             {
                 totalTime = 0;
@@ -76,6 +85,7 @@ public class timer : MonoBehaviour
             }
             else
             {
+                //泥棒を見つけたら（今は仮なのでreturnキーで代用）ゲームクリア画面に遷移する
                 if (Input.GetKeyDown("return"))
                 {
                     timerText.text = "0";
@@ -84,6 +94,7 @@ public class timer : MonoBehaviour
             }
         }
     }
+    //ステージクリア時に加算される変数、clearの値を他のスクリプトで呼び出せるようにする
     public static int c()
     {
         return clear;
